827e1079183d5536c53ba67ef7244a72e4635d76
wsyglwy <1745531041@qq.com>
2023-05-31 23:04:58 +0800

    another try

--- GitBash.sh
+++ GitBash.sh
@@ -7,4 +7,14 @@ git add .
 # git add --all
 
 #  3.查看我们自己的代码，有没有到本地仓库管理中
-git status
+# git status
+
+# cd 进入项目
+# git add . 将本地的暂存
+# git commit -m “描述信息”
+# git config --global user.email “你的邮箱”
+# git config --global user.name “你的名字”
+
+# git push
+
+# small change
